9b2020c8bc007502f794a08721d6d49ac2070044
Elif <elfklc35@gmail.com>
2025-05-04 22:32:35 +0300

    Settings scene added.
    Resolution, music and sfx volumes and fullscreen settings are added.

---
+++ Assets/Scripts/GoToMainMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMainMenu : MonoBehaviour
+{
+    public void GoToMainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
